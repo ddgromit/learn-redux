78df8aae1e934bcddeef24634375d8d38b4a903a
Derek Dahmer <derekdahmer@gmail.com>
2015-09-19 01:12:09 -0700

    Webpack merging

--- tools/webpack/config.js
+++ tools/webpack/config.js
@@ -1,11 +1,10 @@
 import path from 'path';
 import webpack from 'webpack';
+import merge from 'webpack-merge';
 
-export const webpackDevConfig = {
+const common = {
     entry: {
       "client-entry": [
-        "webpack-dev-server/client?http://localhost:8080",
-        "webpack/hot/dev-server",
         "./src/entries/client-entry.js",
       ],
     },
@@ -42,10 +41,6 @@ export const webpackDevConfig = {
       ]
     },
 
-    plugins: [
-      new webpack.HotModuleReplacementPlugin()
-    ],
-
     resolve: {
       root: path.join(__dirname, '../../src'),
       extensions: [
@@ -59,3 +54,16 @@ export const webpackDevConfig = {
       ]
     },
 };
+
+export const webpackDevConfig = merge(common, {
+  entry: {
+    "client-entry": [
+      "webpack-dev-server/client?http://localhost:8080",
+      "webpack/hot/dev-server",
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+});
