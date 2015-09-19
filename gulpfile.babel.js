886798da542fb62af185c50d51f6297f0868692c
Derek Dahmer <derekdahmer@gmail.com>
2015-09-19 00:52:04 -0700

    Create a promise for starting the dev server

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,10 @@
 import gulp from 'gulp';
-import WebpackDevServer from 'webpack-dev-server';
-import webpack from 'webpack';
-import { webpackDevConfig } from './tools/webpack/config';
+import { startWebpackDevServer } from './tools/webpack/dev-server';
 
 gulp.task('default', function() {
-  var compiler = webpack(webpackDevConfig);
-  new WebpackDevServer(compiler, {
-    noInfo: false,
-    historyApiFallback: true,
-    contentBase: './tools/webpack',
-    publicPath: '/dist/',
-    hot: true,
-  }).listen(8080, "localhost", function(err) {
-    if(err) throw err;
-    console.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+  startWebpackDevServer().then(() => {
+    console.log('webpack started');
+  }).catch(() => {
+    console.log("Couldn't start webpack");
   });
 });
---
+++ tools/webpack/dev-server.js
@@ -0,0 +1,22 @@
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack';
+import { webpackDevConfig } from './config';
+
+export function startWebpackDevServer(hot = true) {
+  return new Promise(function(resolve, reject) {
+    var compiler = webpack(webpackDevConfig);
+    new WebpackDevServer(compiler, {
+      noInfo: false,
+      historyApiFallback: true,
+      contentBase: './tools/webpack',
+      publicPath: '/dist/',
+      hot: hot,
+    }).listen(8080, "localhost", function(err) {
+      if (err) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
